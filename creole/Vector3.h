d4c354b9928d527be153e519640d3f14d0e7e74e
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-25 06:44:04 +0200

    adding two needed functions to the vector header

--- creole/Vector3.h
+++ creole/Vector3.h
@@ -5,6 +5,7 @@ class Vector3
 public:
 	float x, y, z;
 	Vector3(): x(0),y(0),z(0) {}
+    Vector3(int x, int y, int z) : x(x), y(y), z(z) {}
 	Vector3(float x, float y, float z): x(x), y(y), z(z) {}
 
     Vector3 operator+(const Vector3& other) const;
@@ -14,5 +15,8 @@ public:
     Vector3 cross(const Vector3& other) const;
     float magnitude() const;
     Vector3 normalized() const;
+    Vector3 project(const Vector3& other) const;
+    Vector3 calculateAngleBetween(const Vector3& other) const;
+
 };
 
