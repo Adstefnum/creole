51f12322cb91fb9a2c81b4ccf44818064764b830
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-25 04:40:05 +0200

    basic vector3 class

--- creole/Vector3.h
+++ creole/Vector3.h
@@ -1,18 +1,18 @@
 #pragma once
+class Vector3
+{
 
-class Vector3 {
 public:
-	Vector3(float x, float y, float z) {
-		m_x = x;
-		m_y = x;
-		m_z = x;
-	}
+	float x, y, z;
+	Vector3(): x(0),y(0),z(0) {}
+	Vector3(float x, float y, float z): x(x), y(y), z(z) {}
 
-	Vector3 normalize() {
-		//
-	}
-
-private:
-	float m_x, m_y, m_z;
-
+    Vector3 operator+(const Vector3& other) const;
+    Vector3 operator-(const Vector3& other) const;
+    Vector3 operator*(float scalar) const;
+    float dot(const Vector3& other) const;
+    Vector3 cross(const Vector3& other) const;
+    float magnitude() const;
+    Vector3 normalized() const;
 };
+
